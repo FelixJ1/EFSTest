3f2595b69e906526d1fc38e63de950093b2b2bb1
Felix Johnson <felixemile23@icloud.com>
2025-10-24 15:51:22 +0100

    b

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+function Home() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello</p>
       </header>
     </div>
+  );
+}
+
+function EventForm() {
+  return <h2>Event Form Page</h2>;
+}
+
+function NotFound() {
+  return <h2>Page Not Found</h2>;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/event-form" element={<EventForm />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
